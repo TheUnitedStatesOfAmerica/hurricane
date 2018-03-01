23996b3479f03ab119305818710536b5e8f14c05
TriggerRimfire <poonikinsthemagicdog@gmail.com>
2018-03-01 15:27:40 +0100

    Basic command loader structure

--- src/Core/Client.ts
+++ src/Core/Client.ts
@@ -2,16 +2,19 @@ import {Collection} from "branches";
 import Command from "../Structures/Base/Command";
 import Supervisor from './Engine/Supervisor';
 import Manager from '../Structures/Manager';
+import CommandHandler from './Engine/Managers/CommandHandler';
 
 // import some water
 
 export default class Client extends Water {
-    public commands: Collection<Command> = new Collection();
-    public supervisor: Supervisor = new Supervisor();
+    public commands: CommandHandler;
     public managers: Collection<Manager>;
+    private supervisor: Supervisor = new Supervisor();
 
     constructor(token: string) {
         super(token);
         this.managers = this.supervisor.loadManagers();
+        this.commands = new CommandHandler();
+        this.commands.loadCommands();
     }
 }
--- src/Core/Engine/Managers/CommandHandler.ts
+++ src/Core/Engine/Managers/CommandHandler.ts
@@ -1,19 +1,34 @@
 import Manager from "../../../Structures/Manager";
 import Client from "../../Client"
 import { Collection } from "branches";
-import Command from "../../../Structures/Base/Command";
+import Command from '../../../Structures/Base/Command';
 
 export default class CommandHandler extends Manager {
-    constructor(client: Client) {
-        super();
-        client.commands = this.loadCommands();
+
+    protected commands: Collection<Command> = new Collection<Command>();
+
+    public loadCommands(): Collection<Command> {
+        return this.commands;
     }
 
-    private loadCommands(): Collection<Command> {
-        return new Collection<Command>();
+    public addCommand(command: Command): Command | Error {
+        if(this.commands.get(command.name)) return new Error('Command already exists!');
+        else {
+            try {
+                this.commands.set(command.name, command);
+                return this.commands.get(command.name);
+            } catch(e) {
+                return new Error('Could not add command!');
+            }
+        }
     }
 
-    public addCommand(): boolean | Error {
-        return true;
+    public getAll(): Collection<Command> {
+        return this.commands;
+    }
+
+    public get(name: Command["name"]): Command | Error {
+        const c = this.commands.get(name);
+        return c ? c : new Error('Could not find command');
     }
 }
