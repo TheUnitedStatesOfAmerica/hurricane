73844c32996d119c0730957e93656857dfbb0701
Zeyla Hellyer <zeylahellyer@gmail.com>
2018-03-11 11:13:22 -0700

    Fix command+category construction

--- src/Core/Engine/CommandHandler.ts
+++ src/Core/Engine/CommandHandler.ts
@@ -30,9 +30,11 @@ export default class CommandHandler extends Manager {
         return this.commands;
     }
 
-    public addCommand(command: Command): Command {
+    public addCommand<T extends Command>(Command: T): Command {
         if(this.commands.get(Command.constructor.name)) throw new Error('Command already exists!');
         try {
+            const command: Command = Command.constructor.call(this.client);
+
             if(command.category) {
                 const category: Category = this.categories.get(command.category.name);
                 if(category) category.commands.set(Command.constructor.name, command);
@@ -45,7 +47,9 @@ export default class CommandHandler extends Manager {
         }
     }
 
-    public addCategory(category: Category): Category {
+    public addCategory(Category: Category): Category {
+        const category = Category.constructor.call(this.client);
+
         this.categories.set(category.name, category);
         return category;
     }
